23c151fcead8e00b8fac6e4c2d0b6208985317ea
Gleb Lysov <lysovg9@gmail.com>
2019-11-22 17:11:20 +0300

    Rerited .sh

--- src/bashsync.sh
+++ src/bashsync.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "bashsync script"
+from=$1
+to=$2
+if [ -z $from ] || [ -z $to ]
+then 
+	echo "Пустая строка"
+	exit 2
+else 
+	echo "Starting to find..."
+fi
+for file in $( find "$from" -mindepth 1 -maxdepth 1)
+do
+	echo $file
+done
